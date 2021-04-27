3451b1d91b6087638b65c33e7f8707bf05b21192
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-04-27 10:31:02 +0300

    new edit

--- js/app.js
+++ js/app.js
@@ -126,12 +126,23 @@ function rendurChart(){
         label: '# of Clicks',
         data: clicks,
         backgroundColor:
-        'rgba(54, 162, 235, 0.2)',
+        'rgba(darkblue)',
         borderColor:
         'rgba(255, 99, 132, 1)',
 
         borderWidth: 1,
-      }]
+      },
+      {
+        label: '# of view',
+        data: shown,
+        backgroundColor:
+        'darkred',
+        borderColor:
+        'rgba(255, 99, 132, 1)',
+
+        borderWidth: 1,
+      }
+      ]
     },
     options: {
       plugins: {
